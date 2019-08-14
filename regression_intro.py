9ec05040c95f7065a785b45411d40d21e4627e5c
TahjidEshan <atahjid@gmail.com>
2019-08-14 23:55:43 +0600

    working 97% accuracy

--- regression_intro.py
+++ regression_intro.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import quandl
 import math
+import numpy as np
+from sklearn import preprocessing, svm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -17,5 +21,19 @@ forcast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 forecast_out = int(math.ceil(0.01*len(df)))
 df['label'] = df[forcast_col].shift(-forecast_out)
+
+X = np.array(df.drop(['label'], 1))
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+x = preprocessing.scale(X)
 df.dropna(inplace = True)
-print(df.head())
+
+y = np.array(df['label'])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+
+clf = LinearRegression(n_jobs = -1)
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
