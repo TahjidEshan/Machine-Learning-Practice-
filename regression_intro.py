4149eb5a125fdd453edd140fbfdd0b8b98319ec1
TahjidEshan <atahjid@gmail.com>
2019-08-14 23:19:16 +0600

    tutorial 3

--- regression_intro.py
+++ regression_intro.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import quandl
+import math
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -11,4 +12,10 @@ df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.
 
 df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
 
+#print(df.head())
+forcast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+forecast_out = int(math.ceil(0.01*len(df)))
+df['label'] = df[forcast_col].shift(-forecast_out)
+df.dropna(inplace = True)
 print(df.head())
